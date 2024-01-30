67f9394b6d42ea6bb3907b5340b5d7652eec7d3a
Mohamad-bader-eddin <mohamad.bader.eddin.1@gmail.com>
2024-01-30 16:14:47 +0400

    implement map iframe

--- src/pages/coverage/views/polygons/views/AddPolygonsContainer.tsx
+++ src/pages/coverage/views/polygons/views/AddPolygonsContainer.tsx
@@ -2,27 +2,37 @@ import PaperContainer from "../../../../../share/components/Paper/PaperContainer
 import Breadcrumb from "../../../../../share/components/breadcrumbs/Breadcrumb";
 import Layout from "../../../../../share/components/layout/Layout";
 import { useTranslation } from "react-i18next";
-import { Typography } from "@mui/material";
-import useAddPolygonValidation from "./hooks/useAddPolygonValidation";
-import AddPolygonsForm from "./components/AddPolygonsForm";
-import GenericAlert from "../../../../../share/components/alert/GenericAlert";
+import { Box, Typography } from "@mui/material";
+// import useAddPolygonValidation from "./hooks/useAddPolygonValidation";
+// import AddPolygonsForm from "./components/AddPolygonsForm";
+// import GenericAlert from "../../../../../share/components/alert/GenericAlert";
+import { useEffect, useRef } from "react";
+import { useDarkMode } from "../../../../../context/DarkMode";
 
 const AddPolygonsContainer = () => {
   const { t } = useTranslation();
-  const {
-    initialValues,
-    onSubmit,
-    validationSchema,
-    errorMsg,
-    openError,
-    setOpenError,
-    msg,
-    openSucsses,
-    setOpenSucsses,
-  } = useAddPolygonValidation();
+  const { darkMode } = useDarkMode();
+  // const {
+  //   initialValues,
+  //   onSubmit,
+  //   validationSchema,
+  //   errorMsg,
+  //   openError,
+  //   setOpenError,
+  //   msg,
+  //   openSucsses,
+  //   setOpenSucsses,
+  // } = useAddPolygonValidation();
   const breadcrumbsTracks = [
     { path: "/dashboard/coverage/polygons", name: t("polygons") },
   ];
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+  useEffect(() => {
+    if (iframeRef.current) {
+      iframeRef.current.contentWindow?.postMessage({ darkMode }, "*");
+    }
+  }, [darkMode]);
+
   return (
     <Layout>
       <Breadcrumb tracks={breadcrumbsTracks} current={t("add_polygon")} />
@@ -30,7 +40,17 @@ const AddPolygonsContainer = () => {
         <Typography variant="h6" sx={{ mb: "20px" }}>
           {t("add_polygon")}
         </Typography>
-        <AddPolygonsForm
+        <Box height={"580px"}>
+          <iframe
+            ref={iframeRef}
+            src="https://tareeq-map.netlify.app/"
+            title="Iframe Title"
+            width={"100%"}
+            height={"557.6px"}
+            style={{ border: "0", background: "transparent" }}
+          />
+        </Box>
+        {/* <AddPolygonsForm
           initialValues={initialValues}
           onSubmit={onSubmit}
           validationSchema={validationSchema}
@@ -46,7 +66,7 @@ const AddPolygonsContainer = () => {
           setOpen={setOpenError}
           type="error"
           msg={errorMsg}
-        />
+        /> */}
       </PaperContainer>
     </Layout>
   );
