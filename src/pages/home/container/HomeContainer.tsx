3d00f60914898cb2069425d40e0e542836223a7a
Mohamad-bader-eddin <mohamad.bader.eddin.1@gmail.com>
2024-03-13 18:31:43 +0300

    implement bird eye UI

--- src/pages/home/container/HomeContainer.tsx
+++ src/pages/home/container/HomeContainer.tsx
@@ -1,4 +1,4 @@
-import { Backdrop, Box, Stack } from "@mui/material";
+import { Backdrop, Box, Stack, Typography } from "@mui/material";
 import Layout from "../../../share/components/layout/Layout";
 import PeopleIcon from "@mui/icons-material/People";
 import MinorCrashOutlinedIcon from "@mui/icons-material/MinorCrashOutlined";
@@ -10,9 +10,14 @@ import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
 import Wedgit from "../components/Wedgit";
 import useHomeQuery from "../hooks/useHomeQuery";
 import Spinner from "../../../share/components/Spinner";
+import PaperContainer from "../../../share/components/Paper/PaperContainer";
+import { useTranslation } from "react-i18next";
+import jsCookie from "js-cookie";
 
 const HomeContainer = () => {
   const { data, isLoading } = useHomeQuery();
+  const { t } = useTranslation();
+  const token = jsCookie.get("accessToken");
   return (
     <Layout>
       {isLoading ? (
@@ -79,6 +84,20 @@ const HomeContainer = () => {
           </Stack>
         </>
       )}
+      <PaperContainer>
+        <Typography variant="h6" sx={{ marginBottom: "15px" }}>
+          {t("bird_eye")}
+        </Typography>
+        <Box height={"400px"}>
+          <iframe
+            src={`https://tareeq-map.netlify.app/#/admin/bird-eye?token=${token}`}
+            title="Iframe Title"
+            width={"100%"}
+            height={"400px"}
+            style={{ border: "0", background: "transparent" }}
+          />
+        </Box>
+      </PaperContainer>
     </Layout>
   );
 };
